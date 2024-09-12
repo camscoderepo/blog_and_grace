6eb7ee6288436cd8abb449038ef19c28eecc0742
camscoderepo <camroman95@gmail.com>
2024-09-11 20:20:36 -0600

    more changes to routes, added routes for slugs and a userStore

--- src/routes/api/blogs/[slug]/+server.ts
+++ src/routes/api/blogs/[slug]/+server.ts
@@ -5,7 +5,7 @@ export async function GET({ params }) {
   const { slug } = params;
 
   // Replace this with actual data fetching logic
-  const blogPost = { slug: slug, title: `Post: ${slug}`, content: 'Blog content goes here...' };
+  const blogPost = { slug: slug, title: `Post: ${slug}`, date: '7-7-2024', content: 'Blog content goes here...' };
 
   return json(blogPost);
 }
