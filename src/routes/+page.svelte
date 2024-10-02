4c918f804092fa94e67005a87d0b9e0eee254bd4
camscoderepo <camroman95@gmail.com>
2024-10-02 14:56:50 -0600

    more changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,7 @@
     /* Add any custom styles here */
   </style>
   
-  <!-- <Navbar slug={slug} /> -->
+  <Navbar slug="blog" />
   
   <main class="container mx-auto p-4 mt-16">
     <h1 class="text-4xl font-bold mb-4">Welcome to Blog & Grace</h1>
